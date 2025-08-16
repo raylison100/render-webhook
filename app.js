b953a340347464d6e84d8397f80d6828875aae2b
Raylison Nunes dos Santos <raylison100@gmail.com>
2025-08-15 21:07:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-// Importar Express.js const express = require ( 'express' );
+// Import Express.js
+const express = require('express');
 
+// Create an Express app
+const app = express();
 
-// Crie um aplicativo Express const app = express ();
+// Middleware to parse JSON bodies
+app.use(express.json());
 
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
 
-// Middleware para analisar corpos JSON 
-app . use ( express . json ());
+// Route for GET requests
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
-// Definir porta e verify_token const port = process . env . PORT || 3000 ; const verifyToken = process . env . VERIFY_TOKEN ;
- 
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
 
+// Route for POST requests
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
 
-// Rota para solicitações GET 
-app . get ( '/' , ( req , res ) => { const { 'hub.mode' : mode , 'hub.challenge' : challenge , 'hub.verify_token' : token } = req . query ;   
-       
-
-  se ( modo === 'inscrever-se' && token === verifyToken ) { 
-    console . log ( 'WEBHOOK VERIFICADO' ); 
-    res . status ( 200 ). enviar ( desafio ); } senão { 
-    res . status ( 403 ). fim (); } });    
-    
-  
-
-
-// Rota para solicitações POST 
-app . post ( '/' , ( req , res ) => { const timestamp = new Date (). toISOString (). replace ( 'T' , ' ' ). slice ( 0 , 19 ); 
-  console . log (` \n\n Webhook recebido $ { timestamp } \n `); 
-  console . log ( JSON . stringify ( req . body , null , 2 )); 
-  res . status ( 200 ). end (); });   
-        
-
-
-// Inicie o aplicativo do servidor . 
-listen ( port , ( ) = > { 
-  console.log (` \n Escutando na porta $ { port } \n` ); }); 
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
